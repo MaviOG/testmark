7f9818db6cff25522b4fcfe6d1b80eeb9fba1e04
MaviOG <mark.roskaric@student.um.si>
2023-01-05 12:54:00 +0100

    mark

--- mark.cs
+++ mark.cs
@@ -16,7 +16,7 @@ PreberiIzDatoteke(pot_do_datoteke, IzpitArray);
 IzracunajStatistiko(statistika, IzpitArray);
 UrediPoOceni(ref IzpitArray);
 ShraniVDatoteko(pot_do_izhodne_datoteke, IzpitArray, statistika);
-Console.Writeline("mark");
+//mark
 
 void ShraniVDatoteko(string pot_do_izhodne_datoteke, Izpit[] IzpitArray, int[] statistika)
 {
